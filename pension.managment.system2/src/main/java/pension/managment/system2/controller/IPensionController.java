954fbd6649aeda2cdc9c3dffb7761c419ba5d1d5
ranjith kumar kv <ranjithkumar1473@gmail.com>
2021-11-24 04:37:48 -0800

    Update IPensionController.java

--- pension.managment.system2/src/main/java/pension/managment/system2/controller/IPensionController.java
+++ pension.managment.system2/src/main/java/pension/managment/system2/controller/IPensionController.java
@@ -5,6 +5,9 @@ package pension.managment.system2.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import pension.managment.system2.controller.IPensionController;
 import pension.managment.system2.model.PensionDetails;
@@ -26,31 +31,46 @@ public class IPensionController {
 	private IPensionService pensionService;
 
 	@PostMapping(path= "/addpension")
-	public PensionDetails addPension(@RequestBody PensionDetails pensionDetails){
+	public ResponseEntity<PensionDetails> addPension(@RequestBody PensionDetails pensionDetails){
 		LOG.info("Adding pension Details");
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "pension details added successfully.");
+		return new ResponseEntity<PensionDetails>(pensionService.addPensionDetails(pensionDetails), headers, HttpStatus.CREATED);
 		
-		return pensionService.addPensionDetails(pensionDetails);
 		
 	}
 	
 	@PutMapping("/updatepension")
-	public PensionDetails updatepension(@RequestBody  PensionDetails pensionDetails ){
+	public ResponseEntity<PensionDetails> updatepension(@RequestBody  PensionDetails pensionDetails ){
 		LOG.info("update pension");
-		return pensionService.updatePensionDetails(pensionDetails);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "pension details updated successfully.");
+		return new ResponseEntity<PensionDetails>(pensionService.updatePensionDetails(pensionDetails), headers, HttpStatus.CREATED);
+		
 				
 	}
 	
 	@GetMapping("/getpensiondetailsbyid/{pensionid}")
-	public PensionDetails viewPension(@PathVariable (name = "pensionid") int pensionid){
+	public ResponseEntity<PensionDetails> viewPension(@PathVariable (name = "pensionid") int pensionid){
 		LOG.info("get pension details");
-		return pensionService.viewPensionDetails(pensionid);
+		PensionDetails pen = pensionService.viewPensionDetails(pensionid);// line
+		LOG.info(pen.toString());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "This pension details is available in the database.");
+		LOG.info(headers.toString());
+		ResponseEntity<PensionDetails> response = new ResponseEntity<PensionDetails>(pen, headers, HttpStatus.OK);
+		return response;
 		
 	}
 	
 	@DeleteMapping("/deletepensionbyid/{pensionid}")
-	public PensionDetails deletePension(@PathVariable int pensionid){
+	public ResponseEntity<PensionDetails> deletePension(@PathVariable int pensionid){
 		LOG.info("Delete pension");
-		return pensionService.deletePensionDetails(pensionid);
+		PensionDetails pen = pensionService.deletePensionDetails(pensionid);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee deleted successfully.");
+		ResponseEntity<PensionDetails> response = new ResponseEntity<PensionDetails>(pen, headers, HttpStatus.OK);
+		return response;
 		
 	}
 }
